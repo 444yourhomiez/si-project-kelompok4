0f2ab6eaf4d5bd802b8c42a312bdf1103cfa3e77
elunggggg <me.khaerulmutakin@gmail.com>
2026-06-19 03:02:40 +0700

    Update terbaru

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Koperasi Motekar</title>
     <link rel="stylesheet" href="{{ asset('adminlte3/plugins/fontawesome-free/css/all.min.css') }}">
     <link rel="stylesheet" href="{{ asset('adminlte3/dist/css/adminlte.min.css') }}">
--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Koperasi Motekar - @yield('title')</title>
     @include('layouts.style')
     @livewireStyles
