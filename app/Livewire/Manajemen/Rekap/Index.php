11f7bfe7b3f939fd61f6e25e55413e28817b5e73
elunggggg <me.khaerulmutakin@gmail.com>
2026-06-18 12:53:22 +0700

    back up 2

--- app/Livewire/Manajemen/Rekap/Index.php
+++ app/Livewire/Manajemen/Rekap/Index.php
@@ -17,9 +17,7 @@ class Index extends Component
         $totalMasuk =
             Simpanan::whereDate('tanggal', $today)
             ->sum('jumlah')
-
             +
-
             Cicilan::whereDate(
                 'tanggal_bayar',
                 $today
@@ -33,103 +31,59 @@ class Index extends Component
             ->where('status', 'aktif')
             ->sum('jumlah_pengajuan');
 
-        $saldo =
-            $totalMasuk - $totalKeluar;
+        $saldo = $totalMasuk - $totalKeluar;
 
-        $simpanan = Simpanan::with('anggota')
-            ->whereDate('tanggal', $today)
-            ->get()
-            ->map(function ($item) {
+        $simpanan = collect(
+            Simpanan::with('anggota')
+                ->whereDate('tanggal', $today)
+                ->get()
+                ->map(function ($item) {
+                    return [
+                        'tanggal' => $item->tanggal,
+                        'kode_anggota' => $item->anggota->kode_anggota ?? '-',
+                        'nama_anggota' => $item->anggota->nama_anggota ?? '-',
+                        'jenis' => 'Simpanan ' . ucfirst($item->jenis_simpanan),
+                        'masuk' => $item->jumlah,
+                        'keluar' => 0,
+                    ];
+                })
+                ->toArray()
+        );
 
-                return [
+        $cicilan = collect(
+            Cicilan::with('pinjaman.anggota')
+                ->whereDate('tanggal_bayar', $today)
+                ->get()
+                ->map(function ($item) {
+                    return [
+                        'tanggal' => $item->tanggal_bayar,
+                        'kode_anggota' => $item->pinjaman?->anggota?->kode_anggota ?? '-',
+                        'nama_anggota' => $item->pinjaman?->anggota?->nama_anggota ?? '-',
+                        'jenis' => 'Cicilan',
+                        'masuk' => $item->jumlah_tagihan,
+                        'keluar' => 0,
+                    ];
+                })
+                ->toArray()
+        );
 
-                    'tanggal' => $item->tanggal,
-
-                    'kode_anggota' =>
-                    $item->anggota->kode_anggota,
-
-                    'nama_anggota' =>
-                    $item->anggota->nama_anggota,
-
-                    'jenis' =>
-                    'Simpanan ' .
-                        ucfirst($item->jenis_simpanan),
-
-                    'masuk' =>
-                    $item->jumlah,
-
-                    'keluar' => 0,
-
-                ];
-            });
-
-        $cicilan = Cicilan::with(
-            'pinjaman.anggota'
-        )
-            ->whereDate(
-                'tanggal_bayar',
-                $today
-            )
-            ->get()
-            ->map(function ($item) {
-
-                return [
-
-                    'tanggal' =>
-                    $item->tanggal_bayar,
-
-                    'kode_anggota' =>
-                    $item->pinjaman
-                        ->anggota
-                        ->kode_anggota,
-
-                    'nama_anggota' =>
-                    $item->pinjaman
-                        ->anggota
-                        ->nama_anggota,
-
-                    'jenis' =>
-                    'Cicilan',
-
-                    'masuk' =>
-                    $item->jumlah_tagihan,
-
-                    'keluar' => 0,
-
-                ];
-            });
-
-        $pinjaman = Pinjaman::with('anggota')
-            ->whereDate(
-                'tanggal_persetujuan',
-                $today
-            )
-            ->where('status', 'aktif')
-            ->get()
-            ->map(function ($item) {
-
-                return [
-
-                    'tanggal' =>
-                    $item->tanggal_persetujuan,
-
-                    'kode_anggota' =>
-                    $item->anggota->kode_anggota,
-
-                    'nama_anggota' =>
-                    $item->anggota->nama_anggota,
-
-                    'jenis' =>
-                    'Pinjaman ' .
-                        ucfirst($item->jenis_pinjaman),
-
-                    'masuk' => 0,
-
-                    'keluar' =>
-                    $item->jumlah_pengajuan,
-
-                ];
-            });
+        $pinjaman = collect(
+            Pinjaman::with('anggota')
+                ->whereDate('tanggal_persetujuan', $today)
+                ->where('status', 'aktif')
+                ->get()
+                ->map(function ($item) {
+                    return [
+                        'tanggal' => $item->tanggal_persetujuan,
+                        'kode_anggota' => $item->anggota->kode_anggota ?? '-',
+                        'nama_anggota' => $item->anggota->nama_anggota ?? '-',
+                        'jenis' => 'Pinjaman ' . ucfirst($item->jenis_pinjaman),
+                        'masuk' => 0,
+                        'keluar' => $item->jumlah_pengajuan,
+                    ];
+                })
+                ->toArray()
+        );
 
         $riwayat = $simpanan
             ->merge($cicilan)
@@ -140,15 +94,10 @@ class Index extends Component
         return view(
             'livewire.manajemen.rekap.index',
             [
-
                 'title' => 'Rekapitulasi Harian',
-
                 'totalMasuk' => $totalMasuk,
-
                 'totalKeluar' => $totalKeluar,
-
                 'saldo' => $saldo,
-
                 'riwayat' => $riwayat,
             ]
         );
