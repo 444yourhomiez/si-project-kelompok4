c6493449e83adb7e5d80713ef614f3112b273bfb
elunggggg <me.khaerulmutakin@gmail.com>
2026-06-24 02:44:09 +0700

    Update terbaru

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ mkdir -p storage/framework/sessions
 mkdir -p storage/framework/views
 mkdir -p storage/framework/cache/data
 mkdir -p storage/logs
-chmod -R 775 storage bootstrap/cache
+chmod -R 777 storage bootstrap/cache
 
 # Clear cache
 php artisan config:clear
